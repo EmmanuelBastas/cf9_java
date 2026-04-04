6ce18243f04e7b94d39cffdc7f13dc58be2af467
Emmanuel Christos Bastas <manos.bastas.ma@gmail.com>
2026-04-04 20:15:11 +0300

    feat: add Builder Design Pattern

---
+++ src/gr/aueb/cf/cf9/ch16/Main.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.cf9.ch16;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book book1 = new Book.Builder(1, "A123").build();
+        Book book2 = new Book.Builder(2, "B456")
+                .author("Tolkien")
+                .title("Lord of the Rings")
+                .build();
+
+        Book book3 = new Book.Builder(3,"C789")
+                .title("The Lost Tales")
+                .build();
+    }
+}
