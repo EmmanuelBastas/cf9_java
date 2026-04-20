6081492baf33959b316307bd003d532869338ee9
Emmanuel Christos Bastas <manos.bastas.ma@gmail.com>
2026-04-20 19:49:13 +0300

    feat: add generic method

--- src/gr/aueb/cf/cf9/ch17/GenericMethods.java
+++ src/gr/aueb/cf/cf9/ch17/GenericMethods.java
@@ -1,5 +1,7 @@
 package gr.aueb.cf.cf9.ch17;
 
+import java.util.List;
+
 public class GenericMethods {
 
     public static void main(String[] args) {
@@ -17,5 +19,21 @@ public class GenericMethods {
         }
     }
 
+    public static void print(GenericNode<?> node) {
+        System.out.println(node.getValue());
+    }
 
+    public static void printNumbers(GenericNode<? extends Number> node) {
+        System.out.println(node.getValue());
+    }
+
+    public static void printNumbers2(GenericNode<? super Integer> node) {
+        System.out.println(node.getValue());
+    }
+
+    private static <T> void produceConsume(List<? super T> list, Iterable<? extends T> src) {
+        for (T item : src) {
+            list.add(item);
+        }
+    }
 }
